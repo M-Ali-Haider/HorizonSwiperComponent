351e981471644c8cf7e1a730e9c752d0dfcbc65d
M-Ali-Haider <mah01134212100@gmail.com>
2023-10-26 22:26:07 +0500

    i am better

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import './ProjectSection.css';
 import proj1 from '../assets/SwiperImager.webp';
 
 function ProjectSection() {
-  const projects = Array(28).fill(proj1);
+  const projects = Array(21).fill(proj1);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleMouseEnter = (index:number) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="project-section-container">
-      {projects.map((project, index) => (
-        <div className="project-unit" key={index}>
-          <img src={project} alt="" />
-        </div>
-      ))}
+        <h1 className="project-section-title">Our Work</h1>
+      <div className="project-section-helper">
+        {projects.map((project, index) => (
+          <div
+            className={`project-unit ${index === activeIndex ? 'increaser' : ''}`}
+            key={index}
+            onMouseEnter={() => handleMouseEnter(index)}
+          >
+            <img src={project} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
